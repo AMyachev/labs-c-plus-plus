0723ffde39c7b7178c69c00e9e24fb93e1012a77
SKholkin <59054308+SKholkin@users.noreply.github.com>
2019-12-23 17:43:02 +0300

    Update ciclestack.h

--- ciclestack.h
+++ ciclestack.h
@@ -10,79 +10,68 @@ template <typename T>  class ciclestack
 		Node(T info) {
 			this->info = info;
 		}
+		void operator=(T info) {
+			this->info = info;
+		}
 	};
 private:
 	Node *first;
 	int length;
-public:
-	T* get_first() {
-		return &(first->info);
+	Node* get_node(int index) {
+		Node *last = first;
+		for (int i = 0; i != index; i++) {
+			last = last->nextnode;
+		}
+		return last;
 	}
-	
+public:
 	int get_length() { return length; }
 	T* get_elem(int index) {
-		Node *search=first;
-		for (int i = 0; i != index; i++) {
-			search = search->nextnode;
-		}
-		return &(search->info);
+		return &(get_node(index)->info);
 	}
 	void push_first(const T &info) {
 		Node *node = new Node(info);
 		if (length == 0) {
 			first = node;
 			first->nextnode = first;
-
 		}
 		else {
 			node->nextnode = first;
 			first = node;
-			last_elem()->nextnode = first;
+			get_node(length)->nextnode = first;
 		}
 		length++;
-		
 	}
 	T* pop_first() {
 		T* firstinfo;
 		Node *myfirst;
 		if (length > 0) {
-			firstinfo = &(first->info);
+			firstinfo = new T(first->info);
 			myfirst = first;
 			first = first->nextnode;
 			length--;
-			last_elem()->nextnode = first;
+			get_node(length)->nextnode = first;
 			delete myfirst;
 			return firstinfo;
 		}
 		else return NULL;
 	}
-	Node* last_elem() {
-		Node *last = first;
-		for (int i = 0; i < length; i++) {
-			last = last->nextnode;
-		}
-		return last;
-	}
-	
 	void deleteelem(int index) {
-		Node *predsearch=first;
-		Node *search = first;
-		int i=0;
 		if (index == 0) {
 			pop_first();
 		}
 		else {
-			for (i = 0; i != index; i++) {
-				predsearch = search;
-				search = search->nextnode;
-				}
-				length--;
-				predsearch->nextnode = search->nextnode;
+			Node *predsearch;
+			Node *search;
+			predsearch = get_node(index - 1);
+			search = predsearch->nextnode;
+			length--;
+			predsearch->nextnode = search->nextnode;
+			delete search;
 		}
 	}
 	ciclestack() {
 		first = nullptr;
-	
 		length = 0;
 	};
 	~ciclestack() {
